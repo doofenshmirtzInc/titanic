4b6183818ac9f08a9cdc8b1c612b0e9933f807eb
jack <darkmatterdontmatter@protonmail.com>
2020-12-24 04:35:03 -0500

    hello world

--- model.py
+++ model.py
@@ -1,5 +1,30 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+
+
+
+#### want to predict who survived the wreck of the titanic based on given data
+
+#### steps to consider
+# understand nature of data .info() .describe()
+# Histograms, boxplots
+# value counts
+# tally missing data
+# correlation between metrics
+# explore interesting themes
+# -- wealthy survive?
+# -- by location
+# -- age scatterplot with ticket price
+# -- young and wealthy variable?
+# -- total spent
+# feature engineering
+# preprocess data together of use a transformer?
+# -- use label for train and test
+# Scaling?
+# model baseline
+# model comparison with cv
+
 
 
 
@@ -8,13 +33,28 @@ import sys
 
 def main():
 
-    df = pd.read_csv( sys.argv[1] )
-    print(df.head)
+    train = pd.read_csv( sys.argv[1] )
+
+    print(train.describe())
+    print(train.info())
+    print(train.columns)
+    # this apparently picks out the number based columns of the data
+    print(train.describe().columns)
+
+    num_col = train[['Age', 'SibSp', 'Parch', 'Fare']]
+    cat_col = train[['Survived', 'Pclass', 'Sex', 'Ticket', 'Cabin', 'Embarked']]
+
+    for i in num_col.columns:
+        plt.hist(num_col[i])
+        plt.title(i)
+        plt.show()
+
+
 
 
 if __name__ == '__main__':
 
-    if( len(sys.argv) ! = 3 ):
+    if( len(sys.argv) != 2 ):
         raise Exception('usage: ./model.py train-file.csv output-file')
 
 
